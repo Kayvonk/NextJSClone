ce77815ff532194508d7f9866dd987401b9c2e24
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-02 11:53:52 -0600

    Deploying on Vercel

--- next.config.js
+++ next.config.js
@@ -4,10 +4,19 @@ module.exports = {
      future: { webpack5: true }
      
   },
-    webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
+  webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config;
+  //   webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback.fs = false;
+  //   }
+  //   return config;
+  // },
 };

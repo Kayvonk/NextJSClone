5b15f50ae66bb50f6bc073b9fa785db8c90605ef
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-02 11:37:25 -0600

    Updated webpack config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ module.exports = {
   images: {
     domains: ["fakestoreapi.com"],
      future: { webpack5: true }
+     
+  },
+    webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
   },
 };

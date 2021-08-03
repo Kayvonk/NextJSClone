c2ca58888d3852247caa9794701bdb5052351eec
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-03 09:25:48 -0600

    Updating build

--- .next/static/oBULBMY--_MV2gUrbJcNf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/259-0d9685145dd6fa94942e.js","static/css/e2ccff8f5ddde8852a39.css","static/chunks/pages/index-7d134650818469ca6fd0.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
